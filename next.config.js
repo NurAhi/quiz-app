5c9fde46bfc4bfc4ce02afc337c1e9ad4cddbe6c
Nur Ahissami <nur.ahissami@wefox.com>
2023-05-14 17:07:16 +0200

    chore(babel): add import react in all files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  reactRequire: true,
   swcMinify: true,
   pageExtensions: ['jsx', 'js', 'tsx', 'ts', 'scss', 'css'],
   devIndicators: {
